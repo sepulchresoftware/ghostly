48d17c3e297c9b130638707a3295e38f96848356
Matthew Fritz <mattf@burbankparanormal.com>
2016-11-13 13:30:14 -0800

    Finished up the database connection code and modified the DB connection test

--- src/Core/Ghostly.php
+++ src/Core/Ghostly.php
@@ -10,7 +10,10 @@
 
 namespace Ghostly\Core;
 
-use PDOException;
+use Ghostly\Exceptions\CannotConnectToDatabaseException;
+
+use \PDO;
+use \PDOException;
 
 class Ghostly
 {
@@ -19,6 +22,7 @@ class Ghostly
 	private $database;
 	private $user;
 	private $pass;
+	private $persistent;
 
 	// the actual database connection through PDO
 	private $pdo;
@@ -32,20 +36,61 @@ class Ghostly
 	 * @param string $host The database server host name or IP
 	 * @param int $port The port number for the database server
 	 * @param string $database The name of the database to use
-	 * @param string $user The username to provide for database server authentication
-	 * @param string $pass The password to provide for database server authentication
+	 * @param string $user The optional username to provide for database server authentication
+	 * @param string $pass The optional password to provide for database server authentication
+	 * @param boolean $persistent The optional parameter for a persistent connection
 	 */
 	public function __construct(
 		$host,
 		$port,
 		$database,
-		$user,
-		$pass
+		$user=null,
+		$pass=null,
+		$persistent=true
 	) {
 		$this->host = $host;
 		$this->port = $port;
 		$this->database = $database;
 		$this->user = $user;
 		$this->pass = $pass;
+		$this->persistent = $persistent;
+
+		// try to create a database connection
+		try {
+			$options = [PDO::FETCH_OBJ => true];
+			if($this->persistent) {
+				$options[PDO::ATTR_PERSISTENT] = true;
+			}
+
+			$this->pdo = new PDO(
+				$this->makeConnectionString(),
+				$this->user,
+				$this->pass,
+				$options
+			);
+		}
+		catch(PDOException $e) {
+			throw new CannotConnectToDatabaseException($e->getMessage());
+		}
+	}
+
+	/**
+	 * Closes the open database connection. If a connection is not open this method
+	 * does nothing.
+	 */
+	public function close() {
+		if(!empty($this->pdo)) {
+			$this->pdo = null;
+		}
+	}
+
+	/**
+	 * Creates and returns the DSN string for a MySQL connection. The values are
+	 * read from the values provided when this class is instantiated.
+	 *
+	 * @return string
+	 */
+	private function makeConnectionString() {
+		return "mysql:host={$this->host};port={$this->port};dbname={$this->database}";
 	}
 }
--- tests/Core/DatabaseConnectionTest.php
+++ tests/Core/DatabaseConnectionTest.php
@@ -8,15 +8,26 @@ use PHPUnit\Framework\TestCase;
 
 class DatabaseConnectionTest extends TestCase
 {
-	public function testDatabaseConnectionWorks() {
-		$orm = new Ghostly(
+	protected $orm;
+
+	public static function setUpBeforeClass() {
+        $this->$orm = new Ghostly(
 			'localhost',
 			'3306',
 			'ghostly-tests',
 			'root',
 			'root'
 		);
+    }
 
+    public static function tearDownAfterClass() {
+        $this->orm->close();
+    }
+
+    /**
+	 * @test
+	 */
+	public function database_connection_is_open() {
 		$this->assertNotEmpty($orm);
 	}
 }
